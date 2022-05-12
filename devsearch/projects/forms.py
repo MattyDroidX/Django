35f18bcbdddac302e860751ce5c93018f7ffdc36
MattyDroidX <fernando.matias.duarte@gmail.com>
2022-05-12 01:29:06 -0300

    styling v2.0

--- devsearch/projects/forms.py
+++ devsearch/projects/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Project
 
 
@@ -6,3 +7,13 @@ class ProjectForm(ModelForm):
     class Meta:
         model = Project
         fields = ['title', 'description', 'feature_image', 'demo_link', 'source_link', 'tags']
+
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __int__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
+        # self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add title'})
