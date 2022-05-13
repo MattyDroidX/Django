69a1cdb947e3b94b4e6688f4fdc56702c64cb685
MattyDroidX <fernando.matias.duarte@gmail.com>
2022-05-13 03:59:28 -0300

    Adding Foreign Key

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -10,6 +10,7 @@ class Profile(models.Model):
                                 null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
+    username = models.CharField(max_length=200, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(max_length=500, blank=True, null=True)
     profile_image = models.ImageField(null=True,
